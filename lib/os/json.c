31608d5ac830d819d4184a4320b99371178bbfbc
Yong Cong Sin <yongcong.sin@gmail.com>
2021-05-15 19:43:48 +0800

    JSON patch for float & num64

--- lib/os/json.c
+++ lib/os/json.c
@@ -790,6 +790,40 @@ static int num_encode(const int32_t *num, json_append_bytes_t append_bytes,
 	return append_bytes(buf, (size_t)ret, data);
 }
 
+static int num64_encode(const int64_t *num, json_append_bytes_t append_bytes,
+		      void *data)
+{
+	char buf[3 * sizeof(int64_t)];
+	int ret;
+
+	ret = snprintk(buf, sizeof(buf), "%lld", *num);
+	if (ret < 0) {
+		return ret;
+	}
+	if (ret >= (int)sizeof(buf)) {
+		return -ENOMEM;
+	}
+
+	return append_bytes(buf, (size_t)ret, data);
+}
+
+static int float_encode(const float *num, json_append_bytes_t append_bytes,
+		      void *data)
+{
+	char buf[3 * sizeof(float)];
+	int ret;
+
+	ret = snprintk(buf, sizeof(buf), "%f", *num);
+	if (ret < 0) {
+		return ret;
+	}
+	if (ret >= (int)sizeof(buf)) {
+		return -ENOMEM;
+	}
+
+	return append_bytes(buf, (size_t)ret, data);
+}
+
 static int bool_encode(const bool *value, json_append_bytes_t append_bytes,
 		       void *data)
 {
@@ -820,6 +854,10 @@ static int encode(const struct json_obj_descr *descr, const void *val,
 				       ptr, append_bytes, data);
 	case JSON_TOK_NUMBER:
 		return num_encode(ptr, append_bytes, data);
+	case JSON_TOK_NUMBER64:
+		return num64_encode(ptr, append_bytes, data);
+	case JSON_TOK_FLOAT:
+		return float_encode(ptr, append_bytes, data);
 	default:
 		return -EINVAL;
 	}
