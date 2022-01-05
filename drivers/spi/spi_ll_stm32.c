b4177edceac726a1ab47508d5e4816217f8dcbd9
Francois Ramu <francois.ramu@st.com>
2022-01-05 16:41:35 +0100

    drivers: spi: stm32 driver with dma checking spi busy status flag
    Defines a function to control the spi busy state during DMA transfer.
    After the DMA Tx, the spi might still have some data to Transmit.
    The driver must wait for the SPI Tx before sending the next packet.
    This is not required for the Rx part as the DMA Rx is already done.
    Some mcus like stm32H7 have a TX complete bit, other must wait for the
    TXE and BSY line.
    
    Signed-off-by: Francois Ramu <francois.ramu@st.com>

--- drivers/spi/spi_ll_stm32.c
+++ drivers/spi/spi_ll_stm32.c
@@ -754,12 +754,8 @@ static int transceive_dma(const struct device *dev,
 		}
 #endif
 
-		/* wait until TX buffer is really empty */
-		while (LL_SPI_IsActiveFlag_TXE(spi) == 0) {
-		}
-
-		/* wait until hardware is really ready */
-		while (LL_SPI_IsActiveFlag_BSY(spi) == 1) {
+		/* wait until spi is no more busy (spi TX fifo is really empty) */
+		while (ll_func_spi_dma_busy(spi) == 0) {
 		}
 
 		LL_SPI_DisableDMAReq_TX(spi);
