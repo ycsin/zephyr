9df85d05c5c3fb6a7218b7e3bfa2ec2b7220b8f5
Yong Cong Sin <ycsin@meta.com>
2023-07-08 22:09:25 +0800

    posix: signal: add initial header
    Initial header for the signal APIs.
    APIs to be implemented in later commit.
    
    Signed-off-by: Yong Cong Sin <ycsin@meta.com>
    
    Update signal.h
    
    Update Kconfig.signal

--- include/zephyr/posix/signal.h
+++ include/zephyr/posix/signal.h
@@ -12,6 +12,52 @@
 extern "C" {
 #endif
 
+#ifdef CONFIG_POSIX_SIGNAL
+#define _NSIG     CONFIG_POSIX_SIGNAL_NSIG
+
+#define SIGHUP    1
+#define SIGINT    2
+#define SIGQUIT   3
+#define SIGILL    4
+#define SIGTRAP   5
+#define SIGABRT   6
+#define SIGBUS    7
+#define SIGFPE    8
+#define SIGKILL   9
+#define SIGUSR1   10
+#define SIGSEGV   11
+#define SIGUSR2   12
+#define SIGPIPE   13
+#define SIGALRM   14
+#define SIGTERM   15
+/* 16 not used */
+#define SIGCHLD   17
+#define SIGCONT   18
+#define SIGSTOP   19
+#define SIGTSTP   20
+#define SIGTTIN   21
+#define SIGTTOU   22
+#define SIGURG    23
+#define SIGXCPU   24
+#define SIGXFSZ   25
+#define SIGVTALRM 26
+#define SIGPROF   27
+/* 28 not used */
+#define SIGPOLL   29
+/* 30 not used */
+#define SIGSYS    31
+#define SIGUNUSED 31
+
+#define SIGRTMIN 32
+#define SIGRTMAX _NSIG
+
+BUILD_ASSERT(CONFIG_POSIX_SIGNAL_NSIG > SIGRTMIN);
+
+typedef struct {
+	unsigned long sig[DIV_ROUND_UP(_NSIG, BITS_PER_LONG)];
+} sigset_t;
+#endif /* CONFIG_POSIX_SIGNAL */
+
 #ifndef SIGEV_NONE
 #define SIGEV_NONE 1
 #endif
