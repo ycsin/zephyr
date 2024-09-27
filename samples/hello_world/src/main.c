ca4d8c372556f2017565a2b767518d33196bff11
Yong Cong Sin <ycsin@meta.com>
2024-09-27 15:41:24 +0800

    wip

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -5,6 +5,17 @@
  */
 
 #include <stdio.h>
+#include <zephyr/devicetree.h>
+
+#define DT_DRV_COMPAT zephyr_ipi_plic
+
+#define IPI_PLIC DT_NODELABEL(ipi_plic)
+DT_INST_NUM_IRQS
+#define IPI_PLIC_CONNECT(n) IRQ_CONNECT(DT_INST_IRQN_BY_IDX(0, n), 1, plic_irq_handler, UINT_TO_POINTER(n), 0)
+
+LISTIFY(DT_NUM_IRQS(IPI_PLIC), IPI_PLIC_CONNECT, (;));
+
+DT_INST_FOREACH_PROP_ELEM(0, interrupts_extended, a)
 
 int main(void)
 {
